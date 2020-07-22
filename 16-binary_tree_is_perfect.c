702c7b407e9e97da858797b849960aa2fd2e79ba
crispthoalex <alex877@gmail.com>
2020-07-22 17:46:13 +0000

    Change conditional tree_is_full to equal in is_perfect function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -52,7 +52,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 
 	if (tree)
 	{
-		if (binary_tree_is_full(tree->left) &&  binary_tree_is_full(tree->right))
+		if (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right))
 		{
 			if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
 			{
