1b370f59de1810940e2426448768627efe94d068
laucavv <laucavv23@gmail.com>
2020-07-22 15:22:49 +0000

    fix betty

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,9 +1,9 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_balance -  function that measures the balance 
+ * binary_tree_balance - function that measures the balance
  * factor of a binary tree
- * @tree: root node of the tree to measure the balance factor 
+ * @tree: root node of the tree to measure the balance factor
  * Return: 2 for balance, -1 for not balnce, otherwise 0
  */
 int binary_tree_balance(const binary_tree_t *tree)
