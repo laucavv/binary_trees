21547e1e9c0c0e475c219717085f1a19f1ecc549
crispthoalex <alex877@gmail.com>
2020-07-22 17:57:29 +0000

    Replace height tree function for size tree function

--- 16-binary_tree_is_perfect.c
+++ 16-binary_tree_is_perfect.c
@@ -17,30 +17,26 @@ int binary_tree_is_full(const binary_tree_t *tree)
 	}
 	return (0);
 }
+
 /**
- * binary_tree_height - function that measures the height of a binary tree
- * @tree:  is a pointer to the root node of the tree to measure the height.
- * Return:  Height of a binary tree, else 0
+ * binary_tree_size - function that measures the size of a binary tree
+ * @tree:  is a pointer to the root node of the tree to measure the size.
+ * Return:  Size of a binary tree, else 0
  */
-size_t binary_tree_height(const binary_tree_t *tree)
+size_t binary_tree_size(const binary_tree_t *tree)
 {
-	size_t count_left = 0, count_right = 0;
+	size_t count = 0;
 
 	if (tree)
 	{
 		if (tree->left)
-			count_left = 1 + binary_tree_height(tree->left);
+			count += binary_tree_size(tree->left);
 		if (tree->right)
-			count_right = 1 + binary_tree_height(tree->right);
-
-		if (count_left > count_right)
-			return (count_left);
-		else
-			return (count_right);
+			count += binary_tree_size(tree->right);
+		count++; /* count root node */
 	}
-	return (0);
+	return (count);
 }
-
 
 /**
  * binary_tree_is_perfect - function that checks if a binary tree is perfect
@@ -54,7 +50,7 @@ int binary_tree_is_perfect(const binary_tree_t *tree)
 	{
 		if (binary_tree_is_full(tree->left) == binary_tree_is_full(tree->right))
 		{
-			if (binary_tree_height(tree->left) == binary_tree_height(tree->right))
+			if (binary_tree_size(tree->left) == binary_tree_size(tree->right))
 			{
 				return (1);
 			}
