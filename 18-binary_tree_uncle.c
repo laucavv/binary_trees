373cb497acf960a1b7e7be75ba7351f07c3ea5d7
laucavv <laucavv23@gmail.com>
2020-07-22 20:51:20 +0000

    add conditional ,if parent exists

--- 18-binary_tree_uncle.c
+++ 18-binary_tree_uncle.c
@@ -7,7 +7,7 @@
  */
 binary_tree_t *binary_tree_uncle(binary_tree_t *node)
 {
-	if (node && node->parent->parent)
+	if (node && node->parent && node->parent->parent)
 	{
 		if (node->parent->parent->left && node->parent->parent->right)
 		{
