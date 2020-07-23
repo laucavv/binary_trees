e782a1bf6f24c4812ee35be10bca2cdb2c12c29d
laucavv <laucavv23@gmail.com>
2020-07-23 23:43:47 +0000

    change name function line 63

--- 101-binary_tree_levelorder.c
+++ 101-binary_tree_levelorder.c
@@ -60,6 +60,6 @@ void binary_tree_levelorder(const binary_tree_t *tree, void (*func)(int))
 
 	for (i = 1; i <= height; i++)
 	{
-		aux_tree_levelorder(tree, i, func);
+		aux_levelorder(tree, i, func);
 	}
 }
